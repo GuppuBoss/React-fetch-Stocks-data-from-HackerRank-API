ea419bc716305a01eb97f932958d33e691d82495
Haseeb Iqbal <haseebiqbal33@gmail.com>
2020-08-16 18:22:39 +0500

    fetch request to get stocks data from API

---
+++ README.md
@@ -0,0 +1,18 @@
+# StocksData
+
+## API to hit
+
+*JSON API URL :*
+`https://jsonmock.hackerrank.com/api/stocks?date=<date>`
+
+## Expected Outputs when result found
+
+- `Open: {open}`
+- `Close: {close}`
+- `High: {high}`
+- `Low: {low}`
+
+## Expected Output when NO results found
+
+In case of no results render below div
+`<div data-testid="no-result">No Results Found</div>`
---
+++ src/components/stock-data/index.js
@@ -0,0 +1,67 @@
+
+import React, { Component } from "react";
+import "./index.css";
+
+export default class StockData extends Component {
+  state = {
+    stockData: null,
+    appInput: ''
+  };
+
+  componentDidMount() {
+    
+  }
+
+  submitInput = (event) => {
+    event.preventDefault();
+    const { appInput } = this.state;
+    // In a real app i keep base api url in enviorment file and create thunk action 
+    // for api requests.
+    let ApiUrl = "https://jsonmock.hackerrank.com/api/stocks?date=";
+    fetch(ApiUrl + appInput)
+    .then(response => response.json())
+    .then(result => {
+      const { data } = result;
+      if(data && !!data.length) {
+        this.setState({
+          stockData: data[0]
+        });
+      } else {
+        this.setState({
+          stockData: null
+        });
+      }
+    });
+  }
+
+  onChange = (event) => {
+    this.setState({
+      appInput: event.target.value
+    });
+  }
+
+  render() {
+    const { stockData } = this.state;
+    return (
+      <div className="layout-column align-items-center mt-50">
+        <section className="layout-row align-items-center justify-content-center">
+          <input type="text" className="large" placeholder="5-January-2000" onChange={this.onChange} id="app-input" data-testid="app-input"/>
+          <button className="" id="submit-button" data-testid="submit-button" onClick={this.submitInput}>Search</button>
+        </section>
+        {
+          stockData ?
+        <ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data" >
+          <li className="py-10">Open: {stockData.open}</li>
+          <li className="py-10">Close: {stockData.close}</li>
+          <li className="py-10">High: {stockData.high}</li>
+          <li className="py-10">Low: {stockData.low}</li>
+        </ul>  : 
+        <div className="mt-50 slide-up-fade-in" id="no-result" data-testid="no-result">
+        { !stockData && "No Results Found" }
+      </div>
+      }
+        
+      </div>
+    );
+  }
+}
